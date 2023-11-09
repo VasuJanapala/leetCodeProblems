f3c58a1f6528e6fd2c8610e1ef8a64e617b73bdf
Vasu Janapala <102677891+VasuDev327@users.noreply.github.com>
2023-11-09 17:46:39 -0600

    updated the code to work for all the different scnarios even for empty items

--- 3. longest substring/longestSubstring.py
+++ 3. longest substring/longestSubstring.py
@@ -1,28 +1,41 @@
+import json
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # here I have created the dictionary to store the tuples with its length and the string respective to that string
         d = {}
         pos = 0
-        for value1 in s:
-            # print(value1)
-            str1 = ''
-            str1 += value1
-            s2 = s[pos+1:]
-            # print("value1: ",value1)
-            for value2 in s2:
-                # print("value2: ",value2)
-                str1 += value2
-                if value2 in str1 or (len(str1) == len(s)):
+        if s!='':
+            for value1 in s:
+                str1 = ''
+                str1 += value1
+                s2 = s[pos+1:]
+                for value2 in s2:
+                    if value2 in str1:
+                        d[value1+str(pos)] = (len(str1), str1)
+                        break
+                    str1 += value2
+                if len(str1) == len(s):
                     d[value1+str(pos)] = (len(str1), str1)
                     break
-                # str1 += value2
-            pos +=1
-        max_value = max(val[0] for val in d.values())
-        print(max_value)
-
+                else:
+                    d[value1+str(pos)] = (len(str1), str1)
+                pos +=1
+            max_value = max(val[0] for val in d.values())
+            return max_value
+        elif s == '':
+            return 0
 
 if __name__ == "__main__":
     sol = Solution()
-    s = "abcabcbb" # yet to test it
-    sol.lengthOfLongestSubstring(s)
-    # print(s[-1])
+    output = {}
+    file_open = open("longestInput.json", "r")
+    data = json.load(file_open)
+    for key, input in data.items():
+        output[input] = sol.lengthOfLongestSubstring(input)
+
+    with open("longestOutput.json", "w") as output_file:
+        json.dump(output, output_file, indent = 4)
+    # s = "aab" 
+    # sol.lengthOfLongestSubstring(s)
