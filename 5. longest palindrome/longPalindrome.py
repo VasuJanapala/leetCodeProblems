0d719b3fb1ab633f972e7919ecc2173515b7b02e
Vasu Janapala <102677891+VasuDev327@users.noreply.github.com>
2023-11-23 07:24:50 -0600

    question 5

---
+++ 5. longest palindrome/longPalindrome.py
@@ -0,0 +1,10 @@
+
+class LPalindrome:
+    def longestPalindrome(self, s: str) -> str:
+        stringIs = s
+        
+
+if __name__ == "__main__":
+    longPalin = LPalindrome()
+    s = "babad"
+    longPalin.longestPalindrome(s)
