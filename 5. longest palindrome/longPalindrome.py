39ff2fdb5ba495b68e3a9e4b9f8575fab1ce5eac
Vasu Janapala <102677891+VasuDev327@users.noreply.github.com>
2023-11-23 19:08:34 -0600

    Updated the code as per different kinds of inputs

--- 5. longest palindrome/longPalindrome.py
+++ 5. longest palindrome/longPalindrome.py
@@ -2,9 +2,31 @@
 class LPalindrome:
     def longestPalindrome(self, s: str) -> str:
         stringIs = s
-        
+        storage = {}
+        pos = 0
+        if stringIs == stringIs[::-1]:
+            return stringIs
+        else:
+            for i in stringIs:
+                stringIsSub = stringIs[pos+1:]
+                dummyString = i
+                for j in stringIsSub:
+                    dummyString += j
+                    if j==i and dummyString == dummyString[::-1]:
+                        storage[dummyString] = len(dummyString)
+                pos += 1
+            # print(storage)
+            if storage:
+                max_key = max(storage, key = storage.get)
+                return max_key
+            else:
+                return stringIs[0]            
 
 if __name__ == "__main__":
     longPalin = LPalindrome()
     s = "babad"
-    longPalin.longestPalindrome(s)
+    s2 = "cbbbbd"
+    s3 = "ccc"
+    s4 = "xaabacxcabaaxcabaax"
+    s5 = "ac"
+    print(longPalin.longestPalindrome(s5))
