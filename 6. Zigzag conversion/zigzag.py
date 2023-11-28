c877913bd7f77a370c036e21439a1c080f3df939
Vasu Janapala <102677891+VasuDev327@users.noreply.github.com>
2023-11-28 10:47:08 -0600

    code is updated with changes those work for hardest cases as well

--- 6. Zigzag conversion/zigzag.py
+++ 6. Zigzag conversion/zigzag.py
@@ -3,43 +3,57 @@ class ZigZag:
     def convert(self, s: str, numRows: int) -> str:
         
         inc_positions = []
+        # print(s)
         converted_string = ''
         pos = 0
         break_value = numRows*2-2
         if ((len(s) == 1) or (numRows == 1) or (len(s) < numRows)):
             return s
         else:
+            while ((len(s))<= (break_value+1)):
+                # print("inside while loop one")
+                s += '$'
+            if (len(s)>(break_value+1)):
+                temp_value = round(len(s)/(break_value+1))
+                while(len(s)<=temp_value*break_value):
+                    # print("inside while loop two")
+                    s += '$'
+            if '.' in s:
             
-            while(pos < len(s)):
-                converted_string += s[pos]
-                inc_positions.append(pos)
-                pos += break_value
-            # print(inc_positions)
-            # print(converted_string)
-            inc = 1
-            while (inc < (numRows - 1)):
-                for i in inc_positions:
-                    
-                    if i == 0:
-                        converted_string += s[i+inc]
-                        if len(inc_positions) == 1:
-                            pass
-
-                    else:
-                        if ((i+inc) < len(s)):
-                            converted_string += s[i-inc]
+                while(pos < len(s)):
+                    # print("inside while loop three")
+                    print(s[pos])
+                    converted_string += s[pos]
+                    inc_positions.append(pos)
+                    pos += break_value
+                inc = 1
+                while (inc < (numRows - 1)):
+                    # print("inside while loop four")
+                    for i in inc_positions:
+                        
+                        if i == 0:
+                            print(s[i+inc])
                             converted_string += s[i+inc]
-                        elif ((i-inc) < len(s)):
-                            converted_string += s[i-inc]
-                inc += 1
-            else:
-                for i in inc_positions:
-                    if ((i+inc) < len(s)):
-                        converted_string += s[i+inc]
-            return(converted_string)
-        # PINALSIGYAHRPI
+                        else:
+                            if ((i+inc) < len(s)):
+                                print(s[i-inc])
+                                print(s[i+inc])
+                                converted_string += s[i-inc]
+                                converted_string += s[i+inc]
+                            elif ((i-inc) < len(s)):
+                                print(s[i-inc])
+                                converted_string += s[i-inc]
+                    inc += 1
+                else:
+                    # print("inside while loop else block")
+                    for i in inc_positions:
+                        if ((i+inc) < len(s)):
+                            converted_string += s[i+inc]
+                converted_string = converted_string.replace('$','')
+                return(converted_string)
+
 if __name__ == '__main__':
     zigzag = ZigZag()
-    str = 'ABCDEFGHI'
-    rows = 6
+    str = "Apalindromeisaword,phrase,number,orothersequenceofunitsthatcanbereadthesamewayineitherdirection,withgeneralallowancesforadjustmentstopunctuationandworddividers."
+    rows = 3
     print(zigzag.convert(str, rows))
