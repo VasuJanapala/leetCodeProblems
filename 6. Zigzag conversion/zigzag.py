067568fd206ac697f716aa8da1a7012fddb8b79a
Vasu Janapala <102677891+VasuDev327@users.noreply.github.com>
2023-11-28 21:41:11 -0600

    minor changes in order to make the code more generalize for the test cases 7 and 8

--- 6. Zigzag conversion/zigzag.py
+++ 6. Zigzag conversion/zigzag.py
@@ -1,9 +1,9 @@
+import json
 
 class ZigZag:
     def convert(self, s: str, numRows: int) -> str:
         
         inc_positions = []
-        # print(s)
         converted_string = ''
         pos = 0
         break_value = numRows*2-2
@@ -11,49 +11,43 @@ class ZigZag:
             return s
         else:
             while ((len(s))<= (break_value+1)):
-                # print("inside while loop one")
                 s += '$'
+
             if (len(s)>(break_value+1)):
-                temp_value = round(len(s)/(break_value+1))
-                while(len(s)<=temp_value*break_value):
-                    # print("inside while loop two")
+                temp_value = round(len(s)/(break_value))
+                while(len(s)<=temp_value*(break_value+1)):
                     s += '$'
-            if '.' in s:
-            
-                while(pos < len(s)):
-                    # print("inside while loop three")
-                    print(s[pos])
-                    converted_string += s[pos]
-                    inc_positions.append(pos)
-                    pos += break_value
-                inc = 1
-                while (inc < (numRows - 1)):
-                    # print("inside while loop four")
-                    for i in inc_positions:
-                        
-                        if i == 0:
-                            print(s[i+inc])
-                            converted_string += s[i+inc]
-                        else:
-                            if ((i+inc) < len(s)):
-                                print(s[i-inc])
-                                print(s[i+inc])
-                                converted_string += s[i-inc]
-                                converted_string += s[i+inc]
-                            elif ((i-inc) < len(s)):
-                                print(s[i-inc])
-                                converted_string += s[i-inc]
-                    inc += 1
-                else:
-                    # print("inside while loop else block")
-                    for i in inc_positions:
+
+            while(pos < len(s)):
+                converted_string += s[pos]
+                inc_positions.append(pos)
+                pos += break_value
+            inc = 1
+            while (inc < (numRows - 1)):
+                for i in inc_positions:
+                    
+                    if i == 0:
+                        converted_string += s[i+inc]
+                    else:
                         if ((i+inc) < len(s)):
+                            converted_string += s[i-inc]
                             converted_string += s[i+inc]
-                converted_string = converted_string.replace('$','')
-                return(converted_string)
+                        elif ((i-inc) < len(s)):
+                            converted_string += s[i-inc]
+                inc += 1
+            else:
+                for i in inc_positions:
+                    if ((i+inc) < len(s)):
+                        converted_string += s[i+inc]
+            converted_string = converted_string.replace('$','')
+            return(converted_string)
 
 if __name__ == '__main__':
     zigzag = ZigZag()
-    str = "Apalindromeisaword,phrase,number,orothersequenceofunitsthatcanbereadthesamewayineitherdirection,withgeneralallowancesforadjustmentstopunctuationandworddividers."
-    rows = 3
-    print(zigzag.convert(str, rows))
+    with open("zigzagInputFile.json", "r") as fopen:
+        data = json.load(fopen)
+    output = {}
+    for key, value in data.items():
+        output["output_of_" + key] = zigzag.convert(value['string'], value['numRows'])
+    with open ("zigzagOutputFile.json", "w") as fwrite:
+        json.dump(output, fwrite, indent=4)
